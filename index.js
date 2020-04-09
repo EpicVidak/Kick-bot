b0c5df69f1d028c63144e4330d67689f77a0fca7
EpicVidak <vidak.kozomara@gmail.com>
2020-04-09 23:16:31 +0200

    add changes

--- index.js
+++ index.js
@@ -1,15 +1,101 @@
-const Discord = require('discord.js');
-const {prefix, token} = require('./auth.json');
-const client = new Discord.Client();
+const discord = require('discord.js');
+const auth = require('./auth.json');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const bot = new discord.Client();
 
-client.on('message', message => {
-    if(message.content.startsWith(`${prefix}poni`)){
-        message.channel.send("!p https://www.youtube.com/watch?v=1ejz1V4OIH0")
-    }    
+bot.once('ready', () => {
+    console.log('Ready!');
 })
 
-client.login(token);
+function payingRespect (guildMember) {
+    const payingRespectNick = 'Paying respect...'
+    let currentNick = guildMember.nickname;
+    if (currentNick === payingRespectNick) 
+        return;
+
+    guildMember.edit({mute: true});
+    guildMember.setNickname(payingRespectNick);
+    setTimeout(() => {
+        guildMember.edit({mute: false})
+        guildMember.setNickname(currentNick);
+    }, 5 * 1000);
+}
+
+bot.on('message',(msg) => {
+    console.log(msg.content);
+    if (msg.content === `${auth.prefix}ping`) {
+        // msg.channel.send('pong');
+        console.log(msg);
+    }
+    if (msg.content === 'X') {
+        msg.channel.send(`${msg.author.username} is doubting you `);
+    }
+    if (msg.content === 'F') {
+        // magicnu llinju koda koja muteuje msg.author
+        const guildMember = msg.member;
+
+        payingRespect(guildMember);
+        guildMember.send('hello!');
+        // let voiceStates = new discord.VoiceState(msg.guild)
+        // voiceStates.setMute(true);
+    }
+    if (msg.content.includes(`${auth.prefix}kick`)) {
+        // console.log(msg);
+        let mention = msg.mentions.users.first();
+        console.log(mention);
+        
+        if (mention) {
+            msg.channel.send(`Why do you want me to kick "${mention.toString()}", are you that salty?`);
+        } else {
+            msg.channel.send(`If you don't specify who to kick, i swear ill kick you ${msg.member.nickname}`);
+        }
+    }
+    checkMessage(msg);
+    
+})
+
+
+function checkMessage (msg) {
+    if (msg.content === `${auth.prefix}bruh`) {
+        if (msg.member.voice.channel) {
+            playSong(msg, './songs/bruh.mp3', 3);
+            setTimeout(() => {
+                msg.member.voice.channel.leave();
+            }, 2000);
+        }
+    }
+    if (msg.content === `${auth.prefix}idegas`) {
+        if (msg.member.voice.channel) {
+            playSong(msg, './songs/idegas.mp3');
+        }
+    }
+    if (msg.content === `${auth.prefix}poni`) {
+        if (msg.member.voice.channel) {
+            playSong(msg, './songs/poni.mp3');
+        }
+    }
+    if (msg.content === `${auth.prefix}udjiuvodu`) {
+        if (msg.member.voice.channel) {
+            playSong(msg, './songs/udjiUVodu.mp3');
+        }
+    }
+    if (msg.content === `${auth.prefix}hasl`) {
+        if (msg.member.voice.channel) {
+            playSong(msg, './songs/hasl.mp3');
+        }
+    }
+}
+
+
+async function playSong(msg, songPath, volume) {
+    volume = volume ? volume : 0.4;
+    let conn = await msg.member.voice.channel.join();
+    await conn.play(`${songPath}`, { volume });
+}
+
+// bot.on("typingStart", function(channel, user){
+//     console.log(user);
+//     channel.send(`${user.tag} has started typing`);
+// });
+
+bot.login(auth.token);
