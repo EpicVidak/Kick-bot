5b41ff519155cdf62133bbdf752034049de377e2
dovahkiin <petar.ilic1337@gmail.com>
2020-04-09 20:13:15 +0200

    new feature

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const {prefix, token} = require('./auth.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login('Njk3ODU4ODE5NzQ0ODU4MTIz.Xo9hYg.tgEj5wBKC4PU7PImzkhIVpmWVrQ');
+client.on('message', message => {
+    if(message.content.startsWith('${prefix}poni')){
+        message.channel.send("!p https://www.youtube.com/watch?v=1ejz1V4OIH0")
+    }    
+})
+
+client.login(token);
