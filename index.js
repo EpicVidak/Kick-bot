6917785009e687f5a6cfe8136a4a8ce2405f95e6
EpicVidak <vidak.kozomara@gmail.com>
2020-04-09 19:02:58 +0200

    fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const auth = require('./auth.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
 });
-
-client.login('Njk3ODU4ODE5NzQ0ODU4MTIz.Xo9hYg.tgEj5wBKC4PU7PImzkhIVpmWVrQ');
+let token = auth.token;
+client.login(token);
