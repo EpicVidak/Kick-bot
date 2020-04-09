8583320aa79e2d4ce86e9257953d70caad5da432
EpicVidak <vidak.kozomara@gmail.com>
2020-04-09 19:42:11 +0200

    reset

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !pig
-            case 'pig':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'oink!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+client.login('token');
